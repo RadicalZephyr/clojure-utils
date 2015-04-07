454e39515e889ffc697bc37885c7060626ed6831
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-07 17:49:30 -0500

    Fix project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Some useful development utilities."
+  :url "https://github.com/RadicalZephyr/clojure-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])

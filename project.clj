3163643144da21e506b24e17d9c2c874f70b7e4f
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-07 17:53:56 -0500

    Change github project name

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject repl-utils "0.1.0-SNAPSHOT"
   :description "Some useful development utilities."
-  :url "https://github.com/RadicalZephyr/clojure-utils"
+  :url "https://github.com/RadicalZephyr/repl-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])

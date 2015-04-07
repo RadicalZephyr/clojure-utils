f4e83e1c5bafd5ccfcf548a855ddd4c280e55423
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-07 18:35:20 -0500

    Correct macroization

--- src/radicalzephyr/repl_utils.clj
+++ src/radicalzephyr/repl_utils.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io :as io]
             [clojure.pprint  :as pp]))
 
-(defmacro redirect [& forms]
+(defmacro redirect [file-name & forms]
   (with-open [wr (clojure.java.io/writer file-name)]
     (binding [*out* wr]
       ~@forms)))
